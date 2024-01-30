3345f5d752bf6c984d0278e0f27b96e521b2742b
Felipe Becker dos Santos <beckerfelipee@gmail.com>
2024-01-30 18:36:45 +0000

    Update build.py

--- build.py
+++ build.py
@@ -109,18 +109,12 @@ def image_with_wiki(pal, place=st):
 # ---------------------------- Web App Build -------------------------- #
 
 # Hide Github Icon
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Header
 with st.container():
